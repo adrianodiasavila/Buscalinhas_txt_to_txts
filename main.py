bfcaddda95402477854a59a5d58ee4c3a61c40a1
Adriano Avila <123967607+adrianodiasavila@users.noreply.github.com>
2023-05-29 11:19:43 -0300

    Foi inserida função input para especificar arquivos e pastas a buscar.

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import os
 
+listfile = str(input('Especifique o arquivo .txt contendo as linhas para buscar: '))
+searchfolder = str(input('Especifique a pasta contendo arquivos .txt onde buscar: '))
 
-with open(r'C:\Users\Adriano\Desktop\DOCUMENTOS\ACADÊMICOS\00 - PESQUISA\REVISÃO DA LITERATURA\STRINGS DE BUSCA\TÍTULOS\TÍTULOS DOS TRABALHOS USADOS.txt', 'r', encoding='utf-8') as f:
+with open(listfile, 'r', encoding='utf-8') as f:
     titulos = [line.strip() for line in f.readlines()]
 
 results = []
 
-for filename in os.listdir(r'C:\Users\Adriano\Desktop\DOCUMENTOS\ACADÊMICOS\00 - PESQUISA\REVISÃO DA LITERATURA\STRINGS DE BUSCA\PESQUISAS FEITAS'):
+for filename in os.listdir(searchfolder):
 
-    with open(f'C:\\Users\\Adriano\\Desktop\\DOCUMENTOS\\ACADÊMICOS\\00 - PESQUISA\REVISÃO DA LITERATURA\\STRINGS DE BUSCA\\PESQUISAS FEITAS\\{filename}', 'r', encoding='utf-8') as file:
+    with open(f'{searchfolder}\\{filename}', 'r', encoding='utf-8') as file:
         file_content = file.read().lower()
 
         file_results = []
